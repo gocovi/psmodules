68ec90ef002623a8db4cb60b08de77097b9ffc31
Jack Musick <jack@gocovi.com>
2020-08-24 16:25:49 -0400

    Create AutomateControlManagement.psm1

---
+++ ConnectWiseAutomate/AutomateControlManagement.psm1
@@ -0,0 +1,119 @@
+<#  
+.SYNOPSIS  
+
+.DESCRIPTION  
+
+.NOTES  
+    File Name  : AutomateControlManagement.ps1
+    Author     : jack@gocovi.com
+    Requires   : 
+
+.LINK 
+#>
+
+$CoviApiKey = $null
+
+function Get-AutomateLatestVersion() {
+    $LTServiceInfo = Get-LTServiceInfo
+    $Server = $LTServiceInfo.'Server Address'.Split("|")[0]
+    $Response = Invoke-RestMethod -Uri "$Server/LabTech/Agent.aspx"
+    return $Response.Replace("||||||", "")
+}
+
+function Write-CoviLog($Status, $Message) {
+    if ($script:CoviApiKey) {
+        $LTServiceInfo = Get-LTServiceInfo
+        $LatestVersion = Get-AutomateLatestVersion
+
+        if (!$Message) {
+            $Message = "None"
+        }
+
+        $Body = [PSCustomObject]@{
+            "service_name"        = "ConnectWise Automate";
+            "service_description" = "Agent Version Check";
+            "sent_from"           = "ConnectWise Control";
+            "short_message"       = "Current Version: $StartVersion   Latest Version: $LatestVersion";
+            "long_message"        = $Message;
+            "trigger_name"        = "Manually ran by Jack Musick";
+            "company_id"          = "$($LTServiceInfo.ClientID)";
+            "computer_name"       = $env:COMPUTERNAME;
+            "computer_id"         = "$($LTServiceInfo.ID)";
+            "location_name"       = "Unknown";
+            "location_id"         = "$LocationID";
+            "status"              = "$Status";
+            "company"             = "Unknown"
+        } | ConvertTo-Json
+
+
+        Invoke-WebRequest `
+            -Uri "https://api.gocovi.com/standard/covi/logs" `
+            -Headers @{ "x-api-key" = $script:CoviApiKey } `
+            -UseBasicParsing `
+            -Method Post `
+            -Body $Body `
+            -ContentType "application/json" | Out-Null
+    }
+}
+
+# Uses LTPosh to compare the server version to the version on the agent.
+function Confirm-AutomateLatestVersion() {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $CoviApiKey,
+
+        [Parameter]
+        [switch]
+        $Update
+    )
+
+    if ($CoviApiKey) {
+        $script:CoviApiKey = $CoviApiKey
+    }
+
+    Write-Host "Importing Labtech Powershell Module..."
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/LabtechConsulting/LabTech-Powershell-Module/master/LabTech.psm1') | Invoke-Expression
+
+    if (!(Get-Service LTService)) {
+        Write-Output "LTService is not installed."
+    }
+    else {
+        $LatestVersion = (Get-AutomateLatestVersion)
+
+        if ((Get-LTServiceInfo).Version -eq $LatestVersion) {
+            Write-Output "This Automate agent is running the latest version."
+        }
+        else {
+            Write-Output "Current Version: $((Get-LTServiceInfo).Version)`nLatest Version: $LatestVersion"
+
+            Write-CoviLog -Status "Warning" -Message "An update is available for this agent."
+
+            if ($Update) {
+                Update-LTService -Confirm:$False
+
+                # Rechecking to make sure the agent got updated.
+                if ((Get-LTServiceInfo).Version -ne $LatestVersion) {
+                    Redo-LTService -Confirm:$False
+                }
+
+                # Checking again after a full reinstall
+                if ((Get-LTServiceInfo).Version -ne $LatestVersion) {
+                    Write-Output "Agent failed to update to the latest version by both updating and reinstalling."
+                    Write-CoviLog -Status "Failed"
+                }
+                else {
+                    Write-Output "Agent updated successfully."
+                    Write-CoviLog -Status "Success"
+                }
+            }
+        }
+    }
+}
+
+$PublicFunctions = @(
+    "Confirm-AutomateLatestVersion"
+)
+
+Export-ModuleMember -Function $PublicFunctions
