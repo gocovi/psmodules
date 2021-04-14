8cdc22dc5becdbb0d37da6897a86350184b2b020
Jack Musick <jack@gocovi.com>
2021-04-14 07:26:16 -0400

    Update AutomateControlManagement.psm1

--- ConnectWiseAutomate/AutomateControlManagement.psm1
+++ ConnectWiseAutomate/AutomateControlManagement.psm1
@@ -198,6 +198,115 @@ function Confirm-AutomateLatestVersion() {
     }
 }
 
+# Uses LTPosh to compare the server version to the version on the agent.
+function Confirm-AutomateLatestVersion() {
+    param (
+        [string]$CoviApiKey,
+
+        [Switch]$Update,
+
+        [Switch]$Force,
+
+        [Switch]$Verbose,
+
+        [string]$Server
+    )
+
+    if ($CoviApiKey) {
+        $script:CoviApiKey = $CoviApiKey
+    }
+
+    try {
+        $LTService = Get-Service | Where-Object { $_.Name -eq "LTService" }
+
+        if (!$LTService) {
+            Write-Output "LTService is not installed."
+        }
+        else {
+            # Making sure service is running
+            if ($LTService.Status -ne "Running") {
+                try {
+                    # Killing the process if it's hung
+                    $LTProcessID = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'LTService'" | Select-Object -ExpandProperty ProcessId
+                    
+                    if ($LTProcessID) {
+                        taskkill /f /pid $LTProcessID
+                    }
+
+                    $LTService | Start-Service -Confirm:$False
+                }
+                catch {
+                    Write-Output "LTService couldn't be started. We will try the update anyways, but this device could have an issue."
+                    Write-CoviLog -Status "Error" -Message "LTService couldn't be started. We will try the update anyways, but this device could have an issue."
+                }
+            }
+
+            Write-Host "Importing Labtech Powershell Module..."
+            (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/LabtechConsulting/LabTech-Powershell-Module/master/LabTech.psm1') | Invoke-Expression
+            Write-Output "Successfully imported module!"
+            
+            $LatestVersion = (Get-AutomateLatestVersion)
+
+            if (!$LatestVersion) {
+                Write-Error "Unable to get the latest version from Automate."
+                exit
+            }
+
+            $LTServiceInfo = Get-LTServiceInfo
+
+            if ($Server) {
+                if ($LTServiceInfo.'Server Address' -notlike "*$Server*") {
+                    Write-Output "Mismatched server found: $($LTServiceInfo.Server)"
+                    Write-CoviLog -Status "Error" -Message "Mismatched server found: $($LTServiceInfo.Server)"
+                    exit
+                }
+            }
+
+            if ($LTServiceInfo.Version -eq $LatestVersion) {
+                Write-Output "This Automate agent is running the latest version."
+
+                # Mostly used to confirm logging works
+                if ($Verbose) {
+                    Write-CoviLog -Status "Verbose" -Message "This Automate agent is running the latest version."
+                }
+            }
+            else {
+                Write-Output "Current Version: $((Get-LTServiceInfo).Version)`nLatest Version: $LatestVersion"
+
+                Write-CoviLog -Status "Information" -Message "An update is available for this agent."
+
+                # Flag to do the actual update.
+                if ($Update) {
+                    Update-LTService -Confirm:$False
+
+                    $Success = Start-UpdateCheckLoop -LatestVersion $LatestVersion
+
+                    # Rechecking to make sure the agent got updated.
+                    if (!$Success -and $Force) {
+                        Redo-LTService -Confirm:$False
+
+                        $Success = Start-UpdateCheckLoop -LatestVersion $LatestVersion
+                    }
+
+                    # Checking again after a full reinstall
+                    if (!$Success) {
+                        Write-Output "Agent failed to update to the latest version."
+                        Write-CoviLog -Status "Failed" -Message "Agent failed to update to the latest version."
+                    }
+                    else {
+                        Write-Output "Agent updated successfully."
+                        Write-CoviLog -Status "Success"
+                    }
+                }
+            }
+        }
+    }
+    catch {
+        Write-Output "Unknown Error: $($_.Exception.Message)"
+        Write-CoviLog -Status "Failed" -Message "Unknown Error: $($_.Exception.Message)"
+    }
+}
+
 $PublicFunctions = @(
     "Confirm-AutomateLatestVersion"
 )
